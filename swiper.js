a8309d65d453422bc5a3549eed5b3b0f03980b91
Matthew Goodson <goody44@users.noreply.github.com>
2015-11-02 22:56:35 +0000

    Little tidy up

--- swiper.js
+++ swiper.js
@@ -1,5 +1,5 @@
 angular.module('swiper', [])
-.directive('swiper', function ($parse) {
+    .directive('swiper', function ($parse) {
   return {
     scope: true,
     link: function(scope, element, attrs) {
@@ -7,20 +7,20 @@ angular.module('swiper', [])
 
       var config = {};
 
-      if ( attrs.auto ) {
-        config.auto = parseInt(attrs.auto,10);
+      if (attrs.auto) {
+        config.auto = parseInt(attrs.auto, 10);
       }
-      if ( attrs.startSlide ) {
-        config.startSlide = parseInt(attrs.startSlide,10);
+      if (attrs.startSlide) {
+        config.startSlide = parseInt(attrs.startSlide, 10);
       }
-      if ( attrs.speed ) {
-        config.speed = parseInt(attrs.speed,10);
+      if (attrs.speed) {
+        config.speed = parseInt(attrs.speed, 10);
       }
-      if ( attrs.onSlideEnd ) {
+      if (attrs.onSlideEnd) {
         var onSlideEnd = $parse(attrs.onSlideEnd);
         config.callback = function(e, index, slide) {
           scope.$apply(function() {
-            onSlideEnd(scope, { index: index, slide: slide});
+            onSlideEnd(scope, {index: index, slide: slide});
           });
         };
       }
